ec915505f152c48bbceb443b6f766bb481028bd3
MisakaTAT <i@mikuac.com>
2026-01-14 14:47:40 +0800

    Initial Commit

---
+++ MicLockd/MicLockdApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicLockdApp.swift
+//  MicLockd
+//
+//  Created by MisakaTAT on 2026/1/14.
+//
+
+import SwiftUI
+
+@main
+struct MicLockdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
